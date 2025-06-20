e26548ded627a0febc51cf9c44799fb83e08abf3
Reginaldo de Lima <bigrege@hotmail.com>
2025-06-20 16:16:36 -0300

    Criação do codigo binding de RestaurantesActivity.kt e layout de restaurantes

--- app/src/main/java/com/reginaldo/ifoodprojeto/RestaurantesActivity.kt
+++ app/src/main/java/com/reginaldo/ifoodprojeto/RestaurantesActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.reginaldo.ifoodprojeto.databinding.ActivityMainBinding
 
 class RestaurantesActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_restaurantes)
+        setContentView(binding.root)
 
     }
 }
